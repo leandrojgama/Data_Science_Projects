ccc34ae6afda4b164ce1378ac28935e21bae45d8
lenon <lenonroschel@gmail.com>
2023-10-30 22:48:23 -0300

    third commit

--- 1_home.py
+++ 1_home.py
@@ -11,7 +11,7 @@ st.set_page_config(
 
 #Carregamento/ tratamento dos dados
 if "data" not in st.session_state:
-    df= pd.read_csv(r"datasets\cardio_train.csv", encoding="utf-8", sep=",", index_col=0)
+    df= pd.read_csv("cardio_train.csv", encoding="utf-8", sep=",", index_col=0)
     
     #Transformando a coluna de idade em anos
     df['age'] = (df['age']/365).round(0).astype('Int64')
@@ -29,7 +29,7 @@ st.sidebar.markdown("Dataset - [Kaggle] (https://www.kaggle.com/datasets/suliano
 #opção para adicionar botão com link
 bt = st.button("codígo fonte - [github]")
 if bt:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/sulianova/cardiovascular-disease-dataset?select=cardio_train.csv")
+    webbrowser.open_new_tab("https://github.com/Lenon1994/Data_Science_Projects/tree/main")
 
 #opção para adicionar texto na página formatado
 st.markdown(
